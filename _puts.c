e386017cba889417370bc3fc90d5470e598b8cfd
simplykevine <umutonikevine0000@gmail.com>
2023-08-21 14:34:54 -0700

    putchar character

--- _puts.c
+++ _puts.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+int _puts(char *string)
+{
+	int idx = 0, char_val = 0;
+
+	if (string)
+	{
+		while (string[idx] != '\0')
+		{
+			print_char(string[idx]);
+		char_val += 1;
+			idx++;
+		}
+	}
+	print_char('\n');
+	return (char_val);
+}
