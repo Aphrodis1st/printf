e1b4b13bd57a657bcd6cdde67626e3dec2db6f81
Aphrodis1st <aphrodisu2019@gmail.com>
2023-08-21 18:47:22 +0200

    string handler

---
+++ print_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int print_string(char *string)
+{
+	int idx = 0, char_val = 0;
+
+	if (string)
+	{
+		while (string[idx] != '\0')
+		{
+			print_char(string[idx]);
+			char_val += 1;
+			idx++;
+		}
+	}
+	return (char_val);
+}
