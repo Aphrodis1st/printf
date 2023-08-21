e8fa80622ae24fa62db23cde73f87eab9602d23a
Aphrodis1st <aphrodisu2019@gmail.com>
2023-08-21 02:21:05 +0200

    Our header file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _HEADER_H_
+#define _AHEADER_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int _printf(const char *format, ...);
+int putx(char *str);
+
+
+#endif
